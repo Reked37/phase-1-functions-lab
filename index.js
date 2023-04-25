9fafaa2bf504768016fa202f3b28654fd0bf0bea
Derek Truesdale <dtruesdale37@gmail.com>
2023-04-25 16:59:40 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+const hq=42
+    if (blocks>42){
+    return blocks-hq
+ }
+    else if (blocks<42){
+    return hq-blocks
+ }
+}
+
+
+function distanceFromHqInFeet(blocks){
+     return distanceFromHqInBlocks(blocks)*264
+}
+
+function distanceTravelledInFeet(startblock, endblock){
+    let blockstravelled=startblock-endblock
+    const absblocks= Math.abs(blockstravelled)
+    return absblocks*264
+}
+
+function calculatesFarePrice(startblock,endblock){
+    let distance=distanceTravelledInFeet(startblock,endblock)
+    if(distance<400){
+        return distance*0
+    }
+    else if(distance>400 && distance<2000){
+        return (distance-400)*.02
+    }
+    else if(distance>2000 && distance<2500){
+        return 25
+    }
+    else if (distance>2500){
+        return 'cannot travel that far'
+    }
+}
